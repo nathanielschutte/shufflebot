1402de5d01a5c3a0ae1debc6f148abaf4d2f03ab
LehighCSE262-Fa21 <lehighcse262fa21@gmail.com>
2021-10-14 15:17:33 -0400

    not implemented

--- shufflebot/aliases.py
+++ shufflebot/aliases.py
@@ -3,16 +3,11 @@
 
 import os
 
-# Config read (using util) to constants
 class Aliases:
-    """Holds a config state"""
+    """Command aliases"""
 
     def __init__(self, file):
         self.file = file
-
-        # try file
-        # fallback to default file
-        # error
 
 # Config defaults for fallback values
 class AliasesFallback:
