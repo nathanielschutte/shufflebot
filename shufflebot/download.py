18d62e059721f5018b3d513f33f6e61e67340add
Nate Schutte <natekschutte@gmail.com>
2021-10-15 00:11:59 -0400

    downloading works

--- shufflebot/download.py
+++ shufflebot/download.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 from validators.utils import ValidationFailure
 import youtube_dl
 import validators
+import os, shutil
 
 from .exceptions import DownloadException 
 
@@ -13,25 +14,29 @@ from .exceptions import DownloadException
 class Downloader:
     """YouTube mp3 downloader"""
 
-    def __init__(self, savedir=None) -> None:
+    def __init__(self, savedir, codec) -> None:
 
         if savedir is not None and savedir[-1] != '/':
             savedir += '/'
+
+        if codec[0] == '.':
+            codec = codec[1:]
 
         self.savedir = savedir
         self.download_status = None
 
         self.opts = {
-            'outtmpl': '%(id)s.%(ext)s',
+            'outtmpl': self.savedir + '%(title)s.%(ext)s',
+            'format': 'worstaudio/worst',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
+                'preferredcodec': codec,
                 'preferredquality': '192',
             }],
+            'prefer_ffmpeg': True,
+            'keepvideo': False,
             'progress_hooks': [self.__status_hook],
         }
-
-        self.ydl = youtube_dl.YoutubeDL(self.opts)
 
     # Find out what video title is associated with the given URL or query string
     async def get_title(self, string) -> str:
@@ -41,19 +46,56 @@ class Downloader:
         print(f'{isUrl=}')
 
         # extract info no download
-        with self.ydl:
+        with youtube_dl.YoutubeDL(self.opts) as ydl:
             if isUrl:
                 try:
-                    result = self.ydl.extract_info(string, download=False)
-                    print(f'URL RESULT: {result["title"]}')
-                    return result['title']
+                    result = ydl.extract_info(string, download=False)
+
+                    # handle playlists
+                    if 'entries' in result:
+                        result = result['entries'][0]
+
+                    print(f'URL RESULT: {result["title"]} {result["id"]}')
+                    return result['title'], result['id']
+
+                except Exception as e:
+                    raise DownloadException(e)
+            else:
+                try:
+                    result = ydl.extract_info(f"ytsearch:{string}", download=False)
+                    if 'entries' in result:
+                        result = result['entries'][0]
+                    
+                    print(f'QUERY RESULT: {result["title"]} {result["id"]}')
+                    return result['title'], result['id']
+
                 except Exception as e:
                     raise DownloadException(e)
 
+    def download_url(self, url):
+        self.__download(url=url)
 
+    def download_query(self, query):
+        self.__download(query=query)
+
+    def download_video(self, string):
+        isUrl = self.__check_url_vs_query(string)
+        if isUrl:
+            self.__download(url=string)
+        else:
+            self.__download(query=string)
+    
+    def __download(self, url=None, query=None) -> str:
+        if url is not None:
+            with youtube_dl.YoutubeDL(self.opts) as ydl:
+                ydl.download([url])
+                return
+        elif query is not None:
+            pass
 
     # Is the string a URL
     def __check_url_vs_query(self, string):
+        string = string.strip()
         
         # multiple words passed but maybe the first one is still a URL
         if string.find(' ') != -1:
